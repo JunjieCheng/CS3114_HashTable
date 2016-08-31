6bf1c3482db2507471396351a467796fecaa7097
Junjie Cheng <cjj741616831@gmail.com>
2016-08-31 17:31:55 -0400

    Handle finished

--- src/HandleTest.java
+++ src/HandleTest.java
@@ -1,32 +1,55 @@
-import static org.junit.Assert.*;
+/**
+ * Test unit of class Handle
+ */
 
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import student.TestCase;
 
 public class HandleTest extends TestCase {
     
-    private Handle h1, h2;
-
+    /**
+     * Objects for testing.
+     */
+    private Handle h1, h2, h3, hNull;
+    
+    /**
+     * Set up.
+     */
     @Before
     public void setUp() throws Exception {
         this.h1 = new Handle(0, 1);
         this.h2 = new Handle(1, 2);
+        this.h3 = new Handle(0, 1);
     }
 
+    /**
+     * Test pos.
+     */
     @Test
     public void testPos() {
         assertEquals(h1.pos(), 0);
         assertEquals(h2.pos(), 1);
     }
     
+    /**
+     * Test len.
+     */
     public void testLen() {
         assertEquals(h1.len(), 1);
         assertEquals(h2.len(), 2);
     }
     
+    /**
+     * Test equals.
+     */
     public void testEquals() {
-        
+        assertFalse(h1.equals(hNull));
+        assertTrue(h1.equals(h1));
+        assertFalse(h1.equals(new Integer(1)));
+        assertFalse(h1.equals(h2));
+        assertTrue(h1.equals(h3));
     }
 
 }
